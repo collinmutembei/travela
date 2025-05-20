de47ba347508d8fb0dbb7bf37fc4f092f7f9bd80
Collin Mutembei <10899666+collinmutembei@users.noreply.github.com>
2025-05-20 14:12:10 +0300

    get or create session

--- backend/ai/agent.py
+++ backend/ai/agent.py
@@ -1,6 +1,6 @@
 from google.adk.agents import Agent
 from google.adk.runners import Runner
-from google.adk.sessions import InMemorySessionService
+from google.adk.sessions import InMemorySessionService, Session
 from google.genai import types
 
 
@@ -21,25 +21,39 @@ root_agent = Agent(
 
 session_service = InMemorySessionService()
 
-
 runner = Runner(
     app_name=settings.app_name,
     agent=root_agent,
     session_service=session_service,
 )
 
+def _get_or_create_session(user_id: str, session_id: str) -> Session:
+    """Get or Create chat session"""
+    session = session_service.get_session(
+        app_name=settings.app_name, user_id=user_id, session_id=session_id
+    )
+    if session is None:
+        session = session_service.create_session(
+            app_name=settings.app_name, user_id=user_id, session_id=session_id
+        )
+    return session
+    
 
 def get_agent_response(user_id: str, user_query: str, session_id: str) -> str:
     """
     Run the Travela agent on the given prompt and return the response text.
     """
-    session_service.create_session(
+    session = session_service.get_session(
         app_name=settings.app_name, user_id=user_id, session_id=session_id
     )
+    if session is None:
+        session = session_service.create_session(
+            app_name=settings.app_name, user_id=user_id, session_id=session_id
+        )
     user_content = types.Content(role="user", parts=[types.Part(text=user_query)])
     final_response_content = "..."
     for event in runner.run(
-        user_id=user_id, session_id=session_id, new_message=user_content
+        user_id=session.user_id, session_id=session.id, new_message=user_content
     ):
         if event.is_final_response() and event.content and event.content.parts:
             final_response_content = event.content.parts[0].text
